12a3bb0fcee6a3bdeafd0fcba4605b9ff8ad42c9
Bill Mill <bill@billmill.org>
2022-09-16 17:24:31 -0400

    add human v bot

---
+++ scripts/human_v_bot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(sys.argv[0]) + "/..")
+
+from go.goboard import GameState, Move
+from go.agent.naive import RandomBot
+from go.gotypes import Player
+from go.utils import print_board, print_move, point_from_coords
+
+
+def main():
+    board_size = 9
+    game = GameState.new_game(board_size)
+    bot = RandomBot()
+
+    while not game.is_over():
+        print(chr(27) + "[2J")
+        print_board(game.board)
+        if game.next_player == Player.black:
+            human_move = input("-- ")
+            point = point_from_coords(human_move.strip())
+            move = Move.play(point)
+        else:
+            move = bot.select_move(game)
+        print_move(game.next_player, move)
+        game = game.apply_move(move)
+
+
+if __name__ == "__main__":
+    main()
